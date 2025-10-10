649b187d25b7fc97e4e32d8762e35a26c4dbc929
Sainathgit config --global user.name Sainathgit config --global user.name Sainath <sainath.valavala.31@gmail.com>
2025-10-10 18:20:31 +0530

    added nodeJS intro

---
+++ nodejs/intro.js
@@ -0,0 +1,12 @@
+//Node.js is an open-source, cross-platform JavaScript runtime environment that allows developers to run JavaScript code outside the browser, mainly on the server side
+
+//nodejs is directly interpreted
+console.log(" welcome to node JS");
+console.log(" welcome to node JS");
+console.log(" welcome to node JS");
+
+let ar=[1,2,3,4,5]
+
+for(var i=0;i<=ar.length-1;i++){
+  console.log(ar[i])
+}
