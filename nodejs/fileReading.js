a18b52a152a86ad39fe13bb2d1c62a074758926c
Sainathgit config --global user.name Sainathgit config --global user.name Sainath <sainath.valavala.31@gmail.com>
2025-10-13 12:18:03 +0530

    added global objects , with filereading

--- nodejs/fileReading.js
+++ nodejs/fileReading.js
@@ -1,7 +1,9 @@
-var fs=require("fs");
+var fs=require("fs");//file system module
+// console.log(fs);
 var data= fs.readFileSync("students.txt");
-// console.log(data);
+console.log(data);
 console.log(data.toString());
-fs.writeFileSync("employees.txt","rahul revanth")
+fs.writeFileSync("employees.txt","rahul revanth ashok")
+fs.writeFileSync("people.txt","ashok shelby john")
 // var n=fs.writeFileSync("employees.txt","rahul revanth")
 // console.log(n.toString())--write files doesnot return anything
--- nodejs/globalObjects.js
+++ nodejs/globalObjects.js
@@ -1,5 +1,19 @@
+// global objects
+console.log()
+process
+__dirname,__filename
+Buffer// data in binary format(memory)
+setTimeout(),clearTimeout(),setInterval(),clearInterval()
+require , module.exports
+console
+setImmediate(), clearImmediate()
+
+
+
 console.log(__dirname);//gives currentt directory
-console.log(__filename);//gives current filename with directory
-console.log(process);
+console.log(__filename);//gives current directory with filename 
+// console.log(process);
 console.log(process.cpuUsage());
 console.log(process.memoryUsage());
+// const buf=Buffer.from("hello");
+// console.log(buf);
