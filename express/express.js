10b51abcc2d5cc5b69d174a499d0745a183c62ea
Sainathgit config --global user.name Sainathgit config --global user.name Sainath <sainath.valavala.31@gmail.com>
2025-10-15 12:27:26 +0530

    added notes related to express.js

--- express/express.js
+++ express/express.js
@@ -1,3 +1,18 @@
+/*expressJS
+- a nodeJS framework to create WebAppn and also provides the server to run that Application.
+-creates a server and receives requests from browser URL and responds back with related data.*/
+
+/*server
+- a computer/system that handles requests and serves responses
+- a platform that u can run the code(Web Appn)
+*/
+
+//application- A full program with logic/code that performs tasks
+//webAppn- retrieving data from database or adding data into database(data base integration)
+
+//website- static webPage
+
+
 const express = require("express");
 // import express from 'express'
 
